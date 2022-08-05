2855f152ad2d2c6cba24929aca0872c90cba7d4a
Anton Honcharov <antonhoncharovv@gmail.com>
2022-08-05 18:08:59 +0300

    add final logic

--- contacts.js
+++ contacts.js
@@ -1,18 +1,12 @@
-// function getCurrentData() {
-//     return Date.now()
-// }
-
 const path = require('path');
 const fs = require('fs').promises;
+const { nanoid } = require('nanoid');
 
 const contactsPath = path.resolve('./db/contacts.json');
 
 async function listContacts() {
-    // fs.readFile(contactsPath, 'utf-8')
-    //     .then(data => console.log(data))
-    //     .catch(error => console.log(error));
-        const data = await fs.readFile(contactsPath, 'utf-8')
-        return JSON.parse(data);   
+    const data = await fs.readFile(contactsPath, 'utf-8')
+    return JSON.parse(data);   
 }
 
 async function getContactById(contactId) {
@@ -23,36 +17,23 @@ async function getContactById(contactId) {
 }
 
 async function removeContact(contactId) {
-        const data = await listContacts();
-        const contactById = data.filter(contact => contact.id !== contactId);
-        
+    const data = await listContacts();
+    const index = data.findIndex(item => item.id === contactId);
+    if (index === -1) { return null };
+    const [result] = data.splice(index, 1);
+    fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+    return result;    
 }
 
-async function addContact(name, email, phone) {
-  const newContact = { id: '11', name, email, phone };
-      const data = await listContacts();
-        data.push(newContact);
-        console.log('newContact', newContact);
-        console.log('data', data);
-        // console.log('result', result);
-        // const contactById = data.filter(contact => contact.id !== contactId);
-    
-    // const newContact = { name, email, phone };
-    // console.log(newContact);
+async function addContact({name, email, phone}) {
+    const newContact = { id: nanoid(), name, email, phone };
+    const data = await listContacts();
+    data.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+    return newContact;
 }
-
-// async function all() {
-//     const data = await listContacts();
-//     console.log("all", data)
-// }
-
-// all();
-// getContactById('55');
-// removeContact('101');
-// addContact('Test', 'test2', "test3")
 
 module.exports = {
-    // getCurrentData,
     listContacts,
     getContactById,
     removeContact,
--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 const { listContacts, getContactById, removeContact, addContact } = require("./contacts");
 const { Command } = require("commander");
 const program = new Command();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+        const contactList = await listContacts(); 
+        console.log(contactList);
+      break;
+
+    case "get":
+        const contactById = await getContactById(id);
+        console.log(contactById);
+      break;
+
+    case "add":
+      const newContact = await addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const contactToDelete = await removeContact(id);
+      console.log(contactToDelete);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
   .option("-n, --name <type>", "user name")
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
-
 program.parse(process.argv);
-
 const argv = program.opts();
-
-async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-          const contactList = await listContacts(); 
-          console.log(contactList);
-      break;
-
-    case "get":
-          const contactById = await getContactById(id);
-          console.log(contactById);
-      break;
-
-    case "add":
-      // ... name email phone
-      break;
-
-    case "remove":
-      // ... id
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
-}
-
-invokeAction({action: "get", id: '1'});
-
+invokeAction(argv);
